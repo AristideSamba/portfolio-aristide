390379ca82d0cb75c0e349900dac19db7d3b6005
AristideSamba <sambaaristide@gmail.com>
2025-02-03 14:50:45 +0100

    Ajout des animations reveal1

--- animation.js
+++ animation.js
@@ -1,44 +1,8 @@
-//Apparition des éléments
-const sr = ScrollReveal();
-sr.reveal('animation-droite', {
-    origin: 'right',
-    distance: '-450px',
-    duration: 3000,
-    delay: 1000
-});
-sr.reveal('.animation-gauche', {
-    origin: 'left',
-    distance: '-450px',
-    duration: 3000,
-    delay: 1000
-});
-sr.reveal('.animation-gauche2', {
-  origin: 'left',
-  distance: '-450px',
-  duration: 4000,
-  delay: 2000
-});
-sr.reveal('.animation-gauche3', {
-  origin: 'left',
-  distance: '-450px',
-  duration: 4000,
-  delay: 2500
-});
-
-const ratio = .1
-const options = {
-  root: null,
-  rootMargin: '0px',
-  threshold: ratio
+function hamburg(){
+  const navbar = document.querySelector(".dropdown")
+  navbar.style.transform = "translateY(0px)"
 }
-const handleIntersect = function (entries, observer) {
-    entries.forEach(function (entry){
-      if (entry.intersectionRatio > ratio) {
-        entry.target.classList.add('reveal-visible')
-        observer.unobserve(entry.target)
-      } 
-    })
-  }
-  
-  const observer = new IntersectionObserver(handleIntersect, options)
-  observer.observe(document.querySelector('.reveal'));
+function cancel(){
+  const navbar = document.querySelector(".dropdown")
+  navbar.style.transform = "translateY(-500px)"
+}
